8042d92ff7a55f82e0d0d486618b83ebc88e16e7
Vyacheslav Kan <vkan745@gmail.com>
2024-06-17 00:20:22 +0500

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,64 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            if (listIterator.nextIndex() == index + 1) {
+                listIterator.add(value);
+                break;
+            }
+            listIterator.next();
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            if (filter.test(listIterator.next())) {
+                listIterator.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            if (filter.test(listIterator.next())) {
+                listIterator.remove();
+                listIterator.add(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> listIterator = list.listIterator();
+
+       while (listIterator.hasNext()) {
+           T value = listIterator.next();
+           ListIterator<T> secondListIterator = elements.listIterator();
+           while (secondListIterator.hasNext()) {
+               if (value == secondListIterator.next()) {
+                   listIterator.remove();
+               }
+           }
+       }
+    }
+}
