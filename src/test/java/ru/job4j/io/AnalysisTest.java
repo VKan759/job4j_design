9073f98d3ff3c4ba208c3cadc064471fa541661e
Vyacheslav Kan <vkan745@gmail.com>
2024-07-04 18:38:05 +0500

    8. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AnalysisTest {
+
+    @Test
+    void check(@TempDir Path tempDir) throws IOException {
+        String source = "data/server.log";
+        File target = tempDir.resolve("tempFile.csv").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source, target.getAbsolutePath());
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(target.getAbsolutePath()))) {
+            bufferedReader.lines().forEach(stringBuilder::append);
+        }
+        assertThat("10:56:01;11:02:02;").isEqualTo(stringBuilder.toString());
+    }
+}
