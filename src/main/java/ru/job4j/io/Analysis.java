a5b4621129aea49ebf297a1cb952dc9995bc2ed6
Vyacheslav Kan <vkan745@gmail.com>
2024-07-06 21:36:34 +0500

    7. Анализ доступности сервера [#859 #503736] (испльзован метод append)

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -4,29 +4,25 @@ import java.io.*;
 
 public class Analysis {
 
-    public String formatToLine(String line, boolean available) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(line.substring(4)).append(";");
-        return available ? stringBuilder.toString() : stringBuilder.append(System.lineSeparator()).toString();
-    }
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-
             String line = reader.readLine();
             boolean available = true;
             boolean isNotAvailable = false;
             boolean status = !line.isEmpty() && !line.startsWith("500") && !line.startsWith(
                     "400") ? available : isNotAvailable;
+
             while (line != null) {
-                if (line.startsWith("400") || line.startsWith("500")) {
+                boolean error = line.startsWith("500") || line.startsWith("400");
+                if (error) {
                     if (status != isNotAvailable) {
-                        writer.write(formatToLine(line, true));
+                        writer.append(line.substring(4)).append(";");
                         status = isNotAvailable;
                     }
                 } else {
                     if (status != available) {
-                        writer.write(formatToLine(line, false));
+                        writer.append(line.substring(4)).append(";").append(System.lineSeparator());
                         status = available;
                     }
                 }
