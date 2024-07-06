a5eeeec5bd889aea7267de0276d2090062e5b8e0
Vyacheslav Kan <vkan745@gmail.com>
2024-07-06 23:15:17 +0500

    7. Анализ доступности сервера [#859 #503736] (код упрощен)

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -7,26 +7,16 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            String line = reader.readLine();
-            boolean available = true;
-            boolean isNotAvailable = false;
-            boolean status = !line.isEmpty() && !line.startsWith("500") && !line.startsWith(
-                    "400") ? available : isNotAvailable;
+            String line;
+            boolean status = true;
 
-            while (line != null) {
-                boolean error = line.startsWith("500") || line.startsWith("400");
-                if (error) {
-                    if (status != isNotAvailable) {
-                        writer.append(line.substring(4)).append(";");
-                        status = isNotAvailable;
-                    }
-                } else {
-                    if (status != available) {
-                        writer.append(line.substring(4)).append(";").append(System.lineSeparator());
-                        status = available;
-                    }
-                }
+            while (reader.ready()) {
                 line = reader.readLine();
+                boolean error = line.startsWith("500") || line.startsWith("400");
+                if (status == error) {
+                    writer.append(line.substring(4)).append(";").append(status ? "" : System.lineSeparator());
+                    status = !status;
+                }
             }
         } catch (
                 IOException e) {
