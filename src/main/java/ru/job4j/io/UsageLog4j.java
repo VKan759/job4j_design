568c44eb473a315ce395d99814f1a1ee336d8e83
Vyacheslav Kan <vkan745@gmail.com>
2024-07-22 18:20:33 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -4,14 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
-
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Vyacheslav";
+        char firstNameWord = 'V';
+        short age = 28;
+        int id = 1111111111;
+        byte groupId = 3;
+        float weight = 75.5F;
+        double height = 163.3;
+        boolean married = true;
+        long number = 82222833888189L;
+
+        LOG.debug("Name: {}. First word: {}. Age: {}. Id: {}. Group id: {}. Weight: {}. Height: {}. Married: {}. Number: {}",
+                name, firstNameWord, age, id, groupId, weight, height, married, number);
     }
 }
