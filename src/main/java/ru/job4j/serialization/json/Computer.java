6fa93b3f6567434fb49433e31ca13d342cd83357
Vyacheslav Kan <vkan745@gmail.com>
2024-07-25 18:25:21 +0500

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/json/Computer.java
+++ src/main/java/ru/job4j/serialization/json/Computer.java
@@ -2,12 +2,34 @@ package ru.job4j.serialization.json;
 
 import java.util.Arrays;
 
+import org.json.*;
+
 public class Computer {
     boolean portable;
     int memoryValue;
     String name;
     Display display;
     String[] components;
+
+    public boolean isPortable() {
+        return portable;
+    }
+
+    public int getMemoryValue() {
+        return memoryValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public String[] getComponents() {
+        return components;
+    }
 
     public Computer(boolean portable, int memoryValue, String name, Display display, String[] components) {
         this.portable = portable;
@@ -26,5 +48,18 @@ public class Computer {
                 + ", display=" + display
                 + ", components=" + Arrays.toString(components)
                 + '}';
+    }
+
+    public static void main(String[] args) {
+        Computer computer = new Computer(true, 512, "Lenovo", new Display("UHD"), new String[]{"Notebook"});
+        JSONObject jsonObject = new JSONObject(computer);
+        JSONObject secondObject = new JSONObject();
+        secondObject.put("portable", computer.isPortable());
+        secondObject.put("memoryValue", computer.getMemoryValue());
+        secondObject.put("name", computer.getName());
+        secondObject.put("display", computer.getDisplay());
+        secondObject.put("components", computer.getComponents());
+        System.out.println(new JSONObject(computer));
+        System.out.println(secondObject);
     }
 }
--- src/main/java/ru/job4j/serialization/json/Display.java
+++ src/main/java/ru/job4j/serialization/json/Display.java
@@ -3,6 +3,10 @@ package ru.job4j.serialization.json;
 public class Display {
     String type;
 
+    public String getType() {
+        return type;
+    }
+
     public Display(String type) {
         this.type = type;
     }
