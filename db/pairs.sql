3051b29dbfb57437453572bd672b2d52012cef64
Vyacheslav Kan <vkan745@gmail.com>
2024-08-06 20:00:34 +0500

    0. Join [#6862 #507927] (откорректировано условие выборки)

--- db/pairs.sql
+++ db/pairs.sql
@@ -10,5 +10,5 @@ values ('Dima', true), ('Katya', false), ('Andrey', true), ('Alisa', false);
 select t1.name, t2.name,  concat(t1.name, ' ', t2.name) as "result" 
 from teens t1
 cross join teens t2
-	where t1.gender is true and t2.gender is false and
-	t1.name != t2.name and t1.gender != t2.gender; 
+	where t1.gender is true and t2.gender is false 
+	and t1.gender != t2.gender; 
